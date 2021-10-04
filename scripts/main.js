7e59bb1ee607e6753e82e58bc9203dc97c25fda0
andr999 <andreykuchay2@gmail.com>
2021-10-04 09:33:17 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+//Кнопка вверх
+$(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.up').fadeIn();
+        } else {
+            $('.up').fadeOut();
+        }
+    });
+    $(function(){
+         $('.up').click(function () {
+    	 $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+        });
+    });
